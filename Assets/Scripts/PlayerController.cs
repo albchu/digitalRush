eddb2f011fe7197d99de8228dace4a91d9a0f9aa
Albert Chu <albertchu539@gmail.com>
2017-06-02 22:32:49 -0600

    Added primitive navigation controls. Added targets to scene for future testing. Added initial animation to player so it doesnt feel so dead

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,31 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-  public float mouseSensitivity = 100.0f;
-  public float clampAngle = 80.0f;
+    public float mouseSensitivity = 100.0f;
+    public float clampAngle = 80.0f;
+    public float movementSpeed = 1f;
+    private float rotY = 0.0f; // rotation around the up/y axis
+    private float rotX = 0.0f; // rotation around the right/x axis
 
-  private float rotY = 0.0f; // rotation around the up/y axis
-  private float rotX = 0.0f; // rotation around the right/x axis
+    void Start()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+    }
 
-  void Start()
-  {
-    Vector3 rot = transform.localRotation.eulerAngles;
-    rotY = rot.y;
-    rotX = rot.x;
-  }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))    // Forward
+        {
+            transform.position += transform.forward * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S))   //Back
+        {
+            transform.position -= transform.forward * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D))   // Right
+        {
+            Vector3 right_angle = Vector3.Cross(transform.forward, Vector3.up);
+            transform.position -= right_angle * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.A))   // Left
+        {
+            Vector3 right_angle = Vector3.Cross(transform.forward, Vector3.up);
+            transform.position += right_angle * Time.deltaTime * movementSpeed;
+        }
 
-  void Update()
-  {
-    float mouseX = Input.GetAxis("Mouse X");
-    float mouseY = -Input.GetAxis("Mouse Y");
 
-    rotY += mouseX * mouseSensitivity * Time.deltaTime;
-    rotX += mouseY * mouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
 
-    rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+        rotY += mouseX * mouseSensitivity * Time.deltaTime;
+        rotX += mouseY * mouseSensitivity * Time.deltaTime;
 
-    Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
-    transform.rotation = localRotation;
-  }
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+    }
 
 }
