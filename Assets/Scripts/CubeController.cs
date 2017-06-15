c10659774a18de861550353a866d88ad98e6f244
Albert Chu <albertchu539@gmail.com>
2017-06-14 22:42:09 -0600

    Started work on level map. Added dynamic golden ratio lighting to cubes

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+    public float movementSpeed = 30f;    // All objects are moving in the z direction towards the camera
+    public float spawnDistance = 75f;   // Units that the object will spawn at
+    public float lightPadding = 1f;     // The offset that lights will spawn over the cube
+    public GameObject cameraObject;
+    public float lightOrbitSpeed = 5f;
+    public GameObject colliderObject;
+    // Use this for initialization
+    void Start () {
+        BoxCollider boxCollider = colliderObject.GetComponent<BoxCollider>();
+        spawnLights(boxCollider);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        moveCube();
+
+    }
+
+    void moveCube()
+    {
+        this.gameObject.transform.position -= transform.forward * Time.deltaTime * movementSpeed;   // Move environment blocks
+
+        if (this.gameObject.transform.position.z < cameraObject.transform.position.z)   // If cube has passed the camera, reset to original position
+        {
+            Vector3 spawnLocation = this.gameObject.transform.position;
+            spawnLocation.z = cameraObject.transform.position.z + spawnDistance;
+            this.gameObject.transform.position = spawnLocation;
+        }
+    }
+
+    /**
+     * Returns a list of colors that obey the golden ratio 
+     * Follows algorithm from http://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
+     */
+    List<Color> getGoldenRatioColors(float numColors=2, float saturation = 1f, float value = 0.95f)
+    {
+        float goldenRatioConjugate = 0.618033988749895f;
+        List<Color> colors = new List<Color>();
+        float hue = Random.Range(0, 1f);
+        for (int i = 0; i < numColors; i++)
+        {
+            hue += goldenRatioConjugate;
+            hue %= 1;
+            Color newColor = Color.HSVToRGB(hue, saturation, value);
+            colors.Add(newColor);
+        }
+        return colors;
+    }
+
+    /**
+     * Spawns light objects on top side of box collider
+     */
+    void spawnLights(BoxCollider boxCollider)
+    {
+        Vector3 lightPosA = this.gameObject.transform.position;
+        Vector3 lightPosB = this.gameObject.transform.position;
+        Vector3 cubeDimensions = boxCollider.size;
+        float lightOffsetY = cubeDimensions.y / 2 + lightPadding;
+        float lightOffsetX = cubeDimensions.x / 2 + lightPadding;
+
+        lightPosA.y += lightOffsetY;
+        lightPosB.y += lightOffsetY;
+
+        lightPosA.x += lightOffsetX;
+        lightPosB.x -= lightOffsetX;
+
+        List<Color> colors = getGoldenRatioColors();
+        Color colorA = colors[0];
+        Color colorB = colors[1];
+        spawnLightAt(lightPosA, colorA, "OrbitLightA");
+        spawnLightAt(lightPosB, colorB, "OrbitLightB");
+    }
+
+    GameObject spawnLightAt(Vector3 spawnPosition, Color color, string lightName="someLight")
+    {
+        GameObject lightGameObject = new GameObject(lightName);
+        Light lightComp = lightGameObject.AddComponent<Light>();
+        lightComp.color = color;
+        lightGameObject.transform.position = spawnPosition;
+        return lightGameObject;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,17 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    public float mouseSensitivity = 100.0f;
-    public float clampAngle = 80.0f;
+
     public float movementSpeed = 1f;
-    private float rotY = 0.0f; // rotation around the up/y axis
-    private float rotX = 0.0f; // rotation around the right/x axis
+    public float cameraDeadzoneY = 5f;
+    public float cameraDeadzoneX = 5f;
+
 
     void Start()
     {
-        Vector3 rot = transform.localRotation.eulerAngles;
-        rotY = rot.y;
-        rotX = rot.x;
     }
 
     void Update()
@@ -36,18 +33,12 @@ public class PlayerController : MonoBehaviour {
             Vector3 right_angle = Vector3.Cross(transform.forward, Vector3.up);
             transform.position += right_angle * Time.deltaTime * movementSpeed;
         }
+        else if (Input.GetKey(KeyCode.Space))   // Left
+        {
+            transform.position += Vector3.up * Time.deltaTime * movementSpeed;
+        }
 
 
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = -Input.GetAxis("Mouse Y");
-
-        rotY += mouseX * mouseSensitivity * Time.deltaTime;
-        rotX += mouseY * mouseSensitivity * Time.deltaTime;
-
-        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
-
-        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
-        transform.rotation = localRotation;
     }
 
 }
