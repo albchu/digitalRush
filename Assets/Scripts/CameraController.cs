08d77f8503ca6049874390347f3427be878d2df1
Albert Chu <albertchu539@gmail.com>
2017-06-17 23:40:41 -0600

    Added slight player rotation on left and right

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,35 +4,16 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float mouseSensitivityY = 200.0f;
-    public float mouseSensitivityX = 100.0f;
-    public float clampAngle = 80.0f;
-    public Transform rotatePoint;
-    private float rotY = 0.0f; // rotation around the up/y axis
-    private float rotX = 0.0f; // rotation around the right/x axis
-                               // Use this for initialization
+    private Quaternion originalRotation;
+
     void Start()
     {
-        Vector3 rot = transform.parent.localRotation.eulerAngles;
-        rotY = rot.y;
-        rotX = rot.x;
+        originalRotation = transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        // Camera Controls
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = -Input.GetAxis("Mouse Y");
-        float deltaY = mouseX * mouseSensitivityY ;
-        rotY += deltaY;
-        rotX += mouseY * mouseSensitivityX * Time.deltaTime;
-
-        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
-
-        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
-        transform.rotation = localRotation;
-        //transform.RotateAround(rotatePoint.position, Vector3.up, deltaY * Time.deltaTime);
+        transform.rotation = originalRotation;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,10 +10,24 @@ public class PlayerController : MonoBehaviour {
     public float dashSpeed = 7f;
     public float dashRestSeconds = 2f;
     public float dashPressedMaxTime = 0.1f;
+    public float rotationRestoreSeconds = 0.5f;     // Time until ship returns to original position after a rotation
+    public float rotationMax = 50f;      // This is the boundary value for how far the ship can rotate when it goes to the left and right
+
+    private float leftRotationAmount;
+    private float rightRotationAmount;
     private float nextDashTime = 0f;
     private float pressedDashTime = 0f;
+    private float rotationStartTime;
+    private bool restoringRotation;       // This value is for whether the restoration process (which can be disrupted) should occur
+    private bool atRestingRotation;       // This value is only for whether the rotation has been completely restored
+    private Quaternion originalRotation;
     void Start()
     {
+        leftRotationAmount = rotationMax;
+        rightRotationAmount = -rotationMax;
+        restoringRotation = false;
+        atRestingRotation = true;
+        originalRotation = transform.rotation;
     }
 
     void Update()
@@ -31,7 +45,7 @@ public class PlayerController : MonoBehaviour {
 
         }
         //print("Time spent pressing dash key is " + pressedDashTime);
-        if (pressedDashTime >= dashPressedMaxTime)
+        if (pressedDashTime >= dashPressedMaxTime)      // We dont want to let the player just hold dash and zoom around like a madman.
         {
             pressedDashTime = 0f;
             nextDashTime = Time.time + dashRestSeconds;
@@ -50,11 +64,72 @@ public class PlayerController : MonoBehaviour {
         {
             Vector3 right_angle = Vector3.Cross(transform.forward, Vector3.up);
             transform.position -= right_angle * Time.deltaTime * movementSpeed * dashModifier;
+
+            // Rotation logic
+            atRestingRotation = false;
+            restoringRotation = false;      // Disable restore so that it does not conflict with this movement
+            transform.RotateAround(transform.position, Vector3.forward, rightRotationAmount * Time.deltaTime);
+            if (rightRotationAmount < 0)
+            {
+                rightRotationAmount++;
+            }
         }
         else if (Input.GetKey(KeyCode.A))   // Left
         {
             Vector3 right_angle = Vector3.Cross(transform.forward, Vector3.up);
             transform.position += right_angle * Time.deltaTime * movementSpeed * dashModifier;
+
+            // Rotation logic
+            atRestingRotation = false;
+            restoringRotation = false;      // Disable restore so that it does not conflict with this movement
+            transform.RotateAround(transform.position, Vector3.forward, leftRotationAmount * Time.deltaTime);
+            if (leftRotationAmount > 0)
+            {
+                leftRotationAmount--;
+            }
+        }
+
+        // Rotations while going left
+        if (Input.GetKeyDown(KeyCode.A) && atRestingRotation)
+        {
+            originalRotation = transform.rotation;  // Save the original rotation
+            rotationStartTime = Time.time;
+            print("recorded original rotation");
+        }
+        
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            //print("Begin restoring rotation");
+            restoringRotation = true;   // Start restoring process
+            leftRotationAmount = rotationMax;
+        }
+
+        // Rotations while going right
+        if (Input.GetKeyDown(KeyCode.D) && atRestingRotation)
+        {
+            originalRotation = transform.rotation;  // Save the original rotation
+            rotationStartTime = Time.time;
+            //print("recorded original rotation");
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            //print("Begin restoring rotation");
+            restoringRotation = true;   // Start restoring process
+            rightRotationAmount = -rotationMax;
+        }
+
+        if (restoringRotation)
+        {
+            float timeSinceStarted = Time.time - rotationStartTime;
+            float percentageComplete = timeSinceStarted / rotationRestoreSeconds;   // Proper way of using lerp/slerp to ensure that full original rotation is achieved
+            transform.rotation = Quaternion.Slerp(transform.rotation, originalRotation, percentageComplete);
+            if (percentageComplete >= 1f || originalRotation == transform.rotation)
+            {
+                //print("Restored original rotation!");
+                restoringRotation = false;
+                atRestingRotation = true;
+            }
         }
     }
 
