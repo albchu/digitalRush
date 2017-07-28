451db4d12f89110c4575f3e1b317c85cd22055bf
Albert Chu <albertchu539@gmail.com>
2017-07-27 20:11:09 -0600

    Pause menu is completed for now with toggling animation. Added some assets to be used as final cube design

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameState.State currentState = GameState.State.Play;
+
+	// Use this for initialization
+	void Start () {
+        currentState = GameState.State.Play;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(currentState == GameState.State.Paused)
+            {
+                currentState = GameState.State.Play;
+            }
+            else if (currentState == GameState.State.Play)
+            {
+                currentState = GameState.State.Paused;
+            }
+            print("GameState is now: " + currentState);
+        }
+	}
+}
---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,11 @@
+﻿
+public class GameState
+{
+    public enum State
+    {
+        Paused,
+        Play,
+        GameOver,
+        Start
+    };
+}
